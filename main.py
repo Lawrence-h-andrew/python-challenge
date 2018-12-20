f197b6b30b4401826bfb4ce77099da128fbfc298
Lawrence Andrew <42677990+Lawrence-h-andrew@users.noreply.github.com>
2018-12-20 13:54:51 -0500

    Backing up my files to Github
    This is incomplete right now!

---
+++ main.py
@@ -0,0 +1,26 @@
+import os
+import csv
+pollCSV = os.path.join('..', 'Python-challenge', 'election_data.csv')
+def getPoll(pollCSV)
+    "Total Number of votes cast"
+    Totalvotes = len(PollCSV[0])
+    "A Complete list of candidates who received votes
+    
+    "Percentage of votes each candidate won"
+
+    "Total votes each candidate won. Use a counter to see how many times each name appears in the file.
+    "Then store each of those values as integers"
+    "make a variable for each candidate percent 'KhanVotes / Totalvotes' "
+
+    "Winner of election based on popular vote"
+
+    print(f"Election Results")
+    print(f"----------------------")
+    print(f"Total Votes: " {str(Totalvotes)})
+    print(f"----------------------")
+    print(f"Khan: " {str(KhanPercent)} str{KhanVotes})
+            "Correy " {str(CorreyPercent)} str{CorreyVotes})
+            "Li : " {str(LiPercent)} str{LiVotes})
+            "O'Tooley " {str(OtooleyPercent} str{OtooleyVotes})
+            "-----------------------")
+    print(f"Winner: " )
