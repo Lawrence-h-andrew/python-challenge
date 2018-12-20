f246fa81f3742f29b0c1bb960d0ade89ae5cc08b
Lawrence Andrew <42677990+Lawrence-h-andrew@users.noreply.github.com>
2018-12-20 18:54:07 -0500

    Changed file from start of class.

--- main.py
+++ main.py
@@ -1,25 +1,34 @@
 import os
 import csv
 "Collect Data from the CSV file saved in Python Challenge"
-budgetCSV = os.path.join('..', 'Python-challenge', 'budget_data.csv')
-"Now I need to define my function"
+csvpath = os.path.join('..', '..', 'budget_data.csv')
+with open(csvpath, 'r',) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter = ',')
+
+    budgetCSV = [] 
+    for row in csvreader:
+        budgetCSV.append(row)
+
+#"Now I need to define my function
 def getBudget(budgetCSV):
     "Total Months? This would be length of data row"
-    totalMonths = len(budgetCSV[0])
+    totalMonths = len(budgetCSV)
     "Net amount over all periods of time"
-    totalProfitLoss = 0
-    totalProfitLoss = totalProfitLoss + int(budgetCSV[1])
+    totalProfit = 0
+    totalProfit = totalProfit + int(budgetCSV[1])
     "Average changes of Profit/Loss of entire time. This wil be sum of data row 1 / total months function we did earlier!"
-    AverageProfitLoss = (int(totalProfitLoss) / (totalMonths))
+    AverageProfitLoss = (int(totalProfit) / (totalMonths))
     "Greatest Increase in Profits so just the largest value in Profit/Loss row"
     GreatestProfit = max(int((budgetcsv[1]))
-    
+#   "Greatest Decrease in Profits"
+    GreatestLoss = min(int((budgetcsv[1]))
 
     print(f"Financial Analysis")
     print(f"$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
     print(f"Total Months: {str(totalMonths)}")
-    print(f"Total: {str(totalProfitLoss)}")
+    print(f"Total: {str(totalProfit)}")
     print(f"Average Change: {str(AverageProfitLoss)}")
     print(f"Greatest Increase in Profits: Feb-2012  {str(GreatestProfit)}")
     print(f"Greatest Decrease in Profits: Sep-2013 {str(GreatestLoss)}")
 
+"Now I have to convert the output printed to a text file..."""
